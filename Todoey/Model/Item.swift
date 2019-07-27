d470ac47fbebb3bbf4d29292ae472210dc8550ad
Ali Farahani <aliofthird@gmail.com>
2019-07-27 13:55:47 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ali Farahani on 2019-07-27.
+//  Copyright © 2019 Ali Farahani. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
